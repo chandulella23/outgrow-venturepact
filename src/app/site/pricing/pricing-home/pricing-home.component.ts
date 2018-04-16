a75bd53ad500d9fcb644c4a8e2561a5a34cbdf40
chandulella <chandulella77@gmail.com>
2018-04-16 14:43:08 +0530

    feat_apr-15th

--- src/app/site/pricing/pricing-home/pricing-home.component.ts
+++ src/app/site/pricing/pricing-home/pricing-home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 
+declare let jQuery: any;
+
 @Component({
   selector: 'app-pricing-home',
   templateUrl: './pricing-home.component.html',
@@ -20,6 +22,57 @@ export class PricingHomeComponent implements OnInit {
   }
 
   ngOnInit() {
+
+    jQuery('.expand-all').click( function(event){
+      if(jQuery(this).hasClass('open')){
+        jQuery('.expand').removeClass('open');
+        jQuery(".detail").slideUp("slow");
+      }
+      else{
+        jQuery('.expand').addClass('open');
+        jQuery(".detail").slideToggle();
+      }
+      jQuery(this).toggleClass('open');
+    });
+
+    jQuery(".expand-rs").click( function(event) {
+      // jQuery(this).parents('.expand').toggleClass('open');
+      // jQuery(this).parents('.expand').find('.detail').slideToggle();
+      
+      var $target = jQuery(event.target);
+      jQuery(this).toggleClass('open');
+      // jQuery(this).parents('.expand').toggleClass('open');
+      jQuery(this).children(".detail").slideToggle();
+  
+      //jQuery('.expand .expand-child > .detail').slideToggle();
+    });
+
+    jQuery(".expand").click( function(event) {
+      var $target = jQuery(event.target);
+      jQuery(this).toggleClass('open');
+      $target.closest(".expand").find(".detail").slideToggle(); 
+    });
+
+    jQuery('.toggle-more').click(function(event){
+      var $target = jQuery(event.target);
+      var d = jQuery(this).children("i").html();
+      if(d === 'add'){
+        jQuery(this).children("i").html('remove');
+        jQuery(this).children("p").html('show less');
+      }
+      else{
+        jQuery(this).children("i").html('add');
+        jQuery(this).children("p").html('show more');
+      }
+      jQuery(this).parents('.pricing-bottom').toggleClass('open');
+      $target.closest(".pricing-bottom").find(".features").slideToggle();  
+    });
+    
+      
+    jQuery('li').click(function() {
+      jQuery(this).addClass('active').siblings().removeClass('active'); 
+    });
+
     this.isActive0 = true;
     this.isActive1 = false;
 
@@ -55,7 +108,7 @@ export class PricingHomeComponent implements OnInit {
     this.add_remove2="add";
   }
 
-
+//Function Cycletab is for selectin Annual or Monthly
   cycleTab(plan) {
     if (plan == "annual") {
       this.isActive0 = true;
@@ -86,145 +139,149 @@ export class PricingHomeComponent implements OnInit {
       console.log("in monthly :is annual is hidden", this.isHideBusi0)
       console.log("in monthly:is monthly is hidden", this.isHideBusi1);
     }
+ }//End of function cycle tab
 
-  }
-  expand(item) {
-    if(item=="in_detail"){
+ //function expand is for expanding&compressing li elements when it's get clciked(Full page Desktop version)
+  // expand(item) {
+  //   if(item=="in_detail"){
 
-      this.isOpen=!this.isOpen;
-      for(let i=0;i<9;i++){
-        this.expand(this.items[i]);
-      }
-    }
-     if(item =="temp"){
-      this.isOpened0 = !this.isOpened0;
-      if (this.isOpened0 == true) {
-        this.dispSec0 = 'block';
-      }
-      else {
-        this.dispSec0 = 'none';
-      }
-    }
-   else if(item =="host"){
-      this.isOpened1 = !this.isOpened1;
-      if (this.isOpened1 == true) {
-        this.dispSec1 = 'block';
-      }
-      else {
-        this.dispSec1 = 'none';
-      }
-    }
-    else if(item =="chart"){
-      this.isOpened2 = !this.isOpened2;
-      if (this.isOpened2 == true) {
-        this.dispSec2 = 'block';
-      }
-      else {
-        this.dispSec2 = 'none';
-      }
-    }
-    else if(item =="cust"){
-      this.isOpened3 = !this.isOpened3;
-      if (this.isOpened3 == true) {
-        this.dispSec3 = 'block';
-      }
-      else {
-        this.dispSec3 = 'none';
-      }
-    }
-    else if(item =="int"){
-      this.isOpened4 = !this.isOpened4;
-      if (this.isOpened4 == true) {
-        this.dispSec4 = 'block';
-      }
-      else {
-        this.dispSec4 = 'none';
-      }
-    }
-    else if(item =="formula"){
-      this.isOpened5 = !this.isOpened5;
-      if (this.isOpened5 == true) {
-        this.dispSec5 = 'block';
-      }
-      else {
-        this.dispSec5 = 'none';
-      }
-    }
-    else if(item =="analytic"){
-      this.isOpened6 = !this.isOpened6;
-      if (this.isOpened6 == true) {
-        this.dispSec6 = 'block';
-      }
-      else {
-        this.dispSec6 = 'none';
-      }
-    }
-    else if(item =="af"){
-      this.isOpened7 = !this.isOpened7;
-      if (this.isOpened7 == true) {
-        this.dispSec7 = 'block';
-      }
-      else {
-        this.dispSec7 = 'none';
-      }
-    }
-    else if(item =="support"){
-      this.isOpened8 = !this.isOpened8;
-      if (this.isOpened8 == true) {
-        this.dispSec8 = 'block';
-      }
-      else {
-        this.dispSec8 = 'none';
-      }
-    }
+  //     this.isOpen=!this.isOpen;
+  //     for(let i=0;i<9;i++){
+  //       this.expand(this.items[i]);
+  //     }
+  //   }
+  //    if(item =="temp"){
+  //     this.isOpened0 = !this.isOpened0;
+  //     if (this.isOpened0 == true) {
+  //       this.dispSec0 = 'block';
+  //     }
+  //     else {
+  //       this.dispSec0 = 'none';
+  //     }
+  //   }
+  //  else if(item =="host"){
+  //     this.isOpened1 = !this.isOpened1;
+  //     if (this.isOpened1 == true) {
+  //       this.dispSec1 = 'block';
+  //     }
+  //     else {
+  //       this.dispSec1 = 'none';
+  //     }
+  //   }
+  //   else if(item =="chart"){
+  //     this.isOpened2 = !this.isOpened2;
+  //     if (this.isOpened2 == true) {
+  //       this.dispSec2 = 'block';
+  //     }
+  //     else {
+  //       this.dispSec2 = 'none';
+  //     }
+  //   }
+  //   else if(item =="cust"){
+  //     this.isOpened3 = !this.isOpened3;
+  //     if (this.isOpened3 == true) {
+  //       this.dispSec3 = 'block';
+  //     }
+  //     else {
+  //       this.dispSec3 = 'none';
+  //     }
+  //   }
+  //   else if(item =="int"){
+  //     this.isOpened4 = !this.isOpened4;
+  //     if (this.isOpened4 == true) {
+  //       this.dispSec4 = 'block';
+  //     }
+  //     else {
+  //       this.dispSec4 = 'none';
+  //     }
+  //   }
+  //   else if(item =="formula"){
+  //     this.isOpened5 = !this.isOpened5;
+  //     if (this.isOpened5 == true) {
+  //       this.dispSec5 = 'block';
+  //     }
+  //     else {
+  //       this.dispSec5 = 'none';
+  //     }
+  //   }
+  //   else if(item =="analytic"){
+  //     this.isOpened6 = !this.isOpened6;
+  //     if (this.isOpened6 == true) {
+  //       this.dispSec6 = 'block';
+  //     }
+  //     else {
+  //       this.dispSec6 = 'none';
+  //     }
+  //   }
+  //   else if(item =="af"){
+  //     this.isOpened7 = !this.isOpened7;
+  //     if (this.isOpened7 == true) {
+  //       this.dispSec7 = 'block';
+  //     }
+  //     else {
+  //       this.dispSec7 = 'none';
+  //     }
+  //   }
+  //   else if(item =="support"){
+  //     this.isOpened8 = !this.isOpened8;
+  //     if (this.isOpened8 == true) {
+  //       this.dispSec8 = 'block';
+  //     }
+  //     else {
+  //       this.dispSec8 = 'none';
+  //     }
+  //   }
     
 
 
-  }
+  // }//End of expand  function for Desktop version
 
+//Function SmallShow is for when we get clciked show more and show less items in mobile version
+  // smallShow(item){
+  //   if(item=="freelancer"){
+  //     this.isSmallOpen0=!this.isSmallOpen0;
+  //     if(this.isSmallOpen0==true){
+  //       this.isSmallDetail0='block';
+  //       this.show0="Show Less";
+  //       this.add_remove0="remove";
+  //     }
+  //     else{
+  //       this.isSmallDetail0='none';
+  //       this.show0="Show More";
+  //       this.add_remove0="add";
+  //     }
+  //   }
+  //   else if(item=="essential"){
+  //     this.isSmallOpen1=!this.isSmallOpen1;
+  //     if(this.isSmallOpen1==true){
+  //       this.isSmallDetail1='block';
+  //       this.show1="Show Less";
+  //       this.add_remove1="remove";
+  //     }
+  //     else{
+  //       this.isSmallDetail1='none';
+  //       this.show1="Show More";
+  //       this.add_remove1="add";
+  //     }
+  //   }
+  //   else if(item=="business"){
 
-  smallShow(item){
-    if(item=="freelancer"){
-      this.isSmallOpen0=!this.isSmallOpen0;
-      if(this.isSmallOpen0==true){
-        this.isSmallDetail0='block';
-        this.show0="Show Less";
-        this.add_remove0="remove";
-      }
-      else{
-        this.isSmallDetail0='none';
-        this.show0="Show More";
-        this.add_remove0="add";
-      }
-    }
-    else if(item=="essential"){
-      this.isSmallOpen1=!this.isSmallOpen1;
-      if(this.isSmallOpen1==true){
-        this.isSmallDetail1='block';
-        this.show1="Show Less";
-        this.add_remove1="remove";
-      }
-      else{
-        this.isSmallDetail1='none';
-        this.show1="Show More";
-        this.add_remove1="add";
-      }
-    }
-    else if(item=="business"){
-
-      this.isSmallOpen2=!this.isSmallOpen2;
-      if(this.isSmallOpen2==true){
-        this.isSmallDetail2='block';
-        this.show2="Show Less";
-        this.add_remove2="remove";
+  //     this.isSmallOpen2=!this.isSmallOpen2;
+  //     if(this.isSmallOpen2==true){
+  //       this.isSmallDetail2='block';
+  //       this.show2="Show Less";
+  //       this.add_remove2="remove";
         
-      }
-      else{
-        this.isSmallDetail2='none';
-        this.show2="Show More";
-        this.add_remove2="add";
-      }
-    }
-  }
+  //     }
+  //     else{
+  //       this.isSmallDetail2='none';
+  //       this.show2="Show More";
+  //       this.add_remove2="add";
+  //     }
+  //   }
+  // }//End of smallShow Function
 
+
+  
 }
+
