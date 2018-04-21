3279cc6bb95fa48581dcbe420d1788e159042d67
chandulella <chandulella77@gmail.com>
2018-04-21 15:57:45 +0530

    hello

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css','../../../../assets/css/sahil-hover.css','../../../../assets/css/home-responsive.css','../../../../assets/css/custom-sa.css']
 })
 export class FooterComponent implements OnInit {
 
